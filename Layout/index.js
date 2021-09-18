100946802eae87d4d8b8dc0f83b3023a79edd0b0
kingkite <kitewebdevelopers@gmail.com>
2021-09-18 09:23:26 +0100

    Removed The Chat Feature

--- Layout/index.js
+++ Layout/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
-import Chat from "../components/Chat";
 
 const Layout = ({ children }) => (
 	<div className="relative h-screen overflow-auto w-screen">
@@ -10,7 +9,6 @@ const Layout = ({ children }) => (
 			{ children }
 		</main>
 		<Footer />
-		<Chat />
 	</div>
 )
 
